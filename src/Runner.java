19b46c7617938abcb429d26e12fbc61e080cb3e2
David Tian <davidtian@Davids-MBP.hitronhub.home>
2018-06-15 18:31:39 -0600

    Project set up with java class

---
+++ src/Runner.java
@@ -0,0 +1,15 @@
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+    /**
+     * This function calculates the angles for each hand of an analog clock, for a time input.
+     *
+     * @param time in seconds, since epoch
+     * @return an array of the angles, in radians, for each hand of an analog clock. [H, M, s] for hours, minutes, seconds
+     */
+    static float[] calculateAngles(int time) {
+        return new float[]{0f, 0f, 0f};
+    }
+}
