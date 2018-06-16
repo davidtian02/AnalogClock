d39450499c647a11417cf652bef4f62078f1c8cd
David Tian <davidtian@Davids-MBP.hitronhub.home>
2018-06-15 19:47:14 -0600

    Adding unit tests for calculating minutes

--- src/RunnerTest.java
+++ src/RunnerTest.java
@@ -49,9 +49,45 @@ public class RunnerTest {
     // ========================== testing minutes ==========================
 
     @Test
-    public void calculateMinuteHandAngle() {
+    public void calculateMinuteHandAngle_withNonZeroValue_shouldReturnAngle() {
+        int time1 = 1;
+        int time2 = 60;
+        float delta = 0.001f;
+        float expected1 = (float) ( 1/(60f * 60) * 2 * Math.PI);
+        float expected2 = (float) (60/(60f * 60) * 2 * Math.PI);
 
+        float result1 = Runner.calculateMinuteHandAngle(time1);
+        float result2 = Runner.calculateMinuteHandAngle(time2);
+
+        assertEquals(expected1, result1, delta);
+        assertEquals(expected2, result2, delta);
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void calculateMinuteHandAngle_withNegativeValue_shouldThrowException() {
+        int time = -1;
+
+        Runner.calculateMinuteHandAngle(time);
+
+        // no - op
+    }
+
+    @Test
+    public void calculateMinuteHandAngle_withOverFullCircle_shouldReturnBaseValue() {
+        int time1 = 3600;
+        int time2 = 3607;
+        float delta = 0.001f;
+        float expected1 = (float) (0/60f * 2 * Math.PI);
+        float expected2 = (float) (7/60f * 2 * Math.PI);
+
+        float result1 = Runner.calculateMinuteHandAngle(time1);
+        float result2 = Runner.calculateMinuteHandAngle(time2);
+
+        assertEquals(expected1, result1, delta);
+        assertEquals(expected2, result2, delta);
+    }
+
+    // ========================== testing hours ==========================
 
     @Test
     public void calculateHourHandAngle() {
